7cfcfa912fc4b0720941eb6d21589cc1dbbaa9fb
SuganyaAsokan <71201772+SuganyaAsokan@users.noreply.github.com>
2020-09-13 18:46:15 +0530

    week1day1-Assigment

---
+++ OddNumber.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class OddNumber {
+
+	public static void main(String[] args) {
+		
+		for(int i=1;i<=20;i++) {
+			if(i%2==1) {
+				System.out.println("odd number between 1 & 20"+i);
+			}
+		}
+
+	}
+
+}
